f95f16f85fe651970948951362f6df44ef5ae997
vasiliy.koryakin <vasiliy.koryakin@gs-labs.ru>
2020-03-25 09:35:32 +0300

    3.3. Разбор первой программы.[#257225]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Vasilii Koriakin");
+        System.out.println("25.03.2020");
+    }
+}
