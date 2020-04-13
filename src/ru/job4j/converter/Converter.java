c16fc3180234a6a4921f6d9d5f9293554cb59a3c
vasiliy.koryakin <vasiliy.koryakin@gs-labs.ru>
2020-04-13 19:59:31 +0300

    5.8. JavaDoc. Code convention[#257256]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -1,16 +1,36 @@
 package ru.job4j.converter;
 
+/**
+ * Convertor
+ * @author Koryakin Vasiliy
+ * @since 27.03.2020
+ * @version 1
+ */
 public class Converter {
+    /**
+     * rubleToEuro, convert rubbles to euros. exchange rate is 70rubles per euro
+     * @param value number of rubles to be converted
+     * @return number of euros. result of conversion
+     */
     public static int rubleToEuro(int value) {
         int rsl = value / 70;
         return rsl;
     }
 
+    /**
+     * rubleToDollar, convert rubbles to dollars. exchange rate is 60rubles per dollar
+     * @param value number of rubles to be converted
+     * @return number of dollars. result of conversion
+     */
     public static int rubleToDollar(int value) {
         int rsl = value / 60;
         return rsl;
     }
 
+    /**
+     * Main, calls methods, compare results with expected values, prints corresponding messages
+     * @param args not used
+     */
     public static void main(String[] args) {
         int in = 140;
         int expected = 2;
@@ -23,10 +43,5 @@ public class Converter {
         out = rubleToDollar(in);
         passed = expected == out;
         System.out.println("180 rubles are 3 Dollars. Test result : " + passed);
-
-//        int euro = Converter.rubleToEuro(140);
-//        System.out.println("140 rubles are " + euro + " euro.");
-//        int dollar = Converter.rubleToDollar(140);
-//        System.out.println("140 rubles are " + dollar + " dollar.");
     }
 }
