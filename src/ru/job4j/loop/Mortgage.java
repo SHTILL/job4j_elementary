0c102d676a7ac0aa26075cde9f9ed57fb8dd48f1
vasiliy.koryakin <vasiliy.koryakin@gs-labs.ru>
2020-04-14 19:51:35 +0300

    5.7. Ипотека.[#257248]

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -2,9 +2,9 @@ package ru.job4j.loop;
 
 public class Mortgage {
     public int year(double amount, int salary, double percent) {
-        int year = 1;
+        int year = 0;
 
-        while ((amount * (1 + percent / 100)) > salary) {
+        while (amount > 0) {
             amount = (amount * (1 + percent / 100)) - salary;
             year++;
         }
