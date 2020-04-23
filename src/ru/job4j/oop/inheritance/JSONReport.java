9785693ee1da6d0bada0c9846015ed793019f378
vasiliy.koryakin <vasiliy.koryakin@gs-labs.ru>
2020-04-23 17:36:10 +0300

    4. Переопределение[#257347]

--- src/ru/job4j/oop/inheritance/JSONReport.java
+++ src/ru/job4j/oop/inheritance/JSONReport.java
@@ -3,6 +3,8 @@ package ru.job4j.oop.inheritance;
 public class JSONReport extends TextReport {
     @Override
     public String generate(String name, String body) {
-        return "{\n name: " + name + "\n body: " + body + "\n}";
+        return  "{" + System.lineSeparator()
+                + " name: " + name + System.lineSeparator()
+                + " body: " + body + System.lineSeparator() + "}";
     }
 }
